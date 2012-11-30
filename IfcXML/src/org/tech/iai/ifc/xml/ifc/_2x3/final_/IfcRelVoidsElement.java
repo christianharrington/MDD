b379ff60660cfacdf75566ea9008b3a3a4353a6b
Thomas Didriksen <t.didriksen@gmail.com>
2012-11-30 12:45:59 +0100

    Fixed DanglingHREFException on RelatingBuildingElement by setting reference as containment

--- IfcXML/src/org/tech/iai/ifc/xml/ifc/_2x3/final_/IfcRelVoidsElement.java
+++ IfcXML/src/org/tech/iai/ifc/xml/ifc/_2x3/final_/IfcRelVoidsElement.java
@@ -22,26 +22,27 @@ package org.tech.iai.ifc.xml.ifc._2x3.final_;
  */
 public interface IfcRelVoidsElement extends IfcRelConnects {
 	/**
-	 * Returns the value of the '<em><b>Relating Building Element</b></em>' reference.
+	 * Returns the value of the '<em><b>Relating Building Element</b></em>' containment reference.
 	 * <!-- begin-user-doc -->
 	 * <p>
 	 * If the meaning of the '<em>Relating Building Element</em>' reference isn't clear,
 	 * there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
-	 * @return the value of the '<em>Relating Building Element</em>' reference.
+	 * @return the value of the '<em>Relating Building Element</em>' containment reference.
 	 * @see #setRelatingBuildingElement(RelatingBuildingElementType)
 	 * @see org.tech.iai.ifc.xml.ifc._2x3.final_.FinalPackage#getIfcRelVoidsElement_RelatingBuildingElement()
-	 * @model extendedMetaData="kind='element' name='RelatingBuildingElement' namespace='##targetNamespace'"
+	 * @model containment="true"
+	 *        extendedMetaData="kind='element' name='RelatingBuildingElement' namespace='##targetNamespace'"
 	 * @generated
 	 */
 	RelatingBuildingElementType getRelatingBuildingElement();
 
 	/**
-	 * Sets the value of the '{@link org.tech.iai.ifc.xml.ifc._2x3.final_.IfcRelVoidsElement#getRelatingBuildingElement <em>Relating Building Element</em>}' reference.
+	 * Sets the value of the '{@link org.tech.iai.ifc.xml.ifc._2x3.final_.IfcRelVoidsElement#getRelatingBuildingElement <em>Relating Building Element</em>}' containment reference.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Relating Building Element</em>' reference.
+	 * @param value the new value of the '<em>Relating Building Element</em>' containment reference.
 	 * @see #getRelatingBuildingElement()
 	 * @generated
 	 */
